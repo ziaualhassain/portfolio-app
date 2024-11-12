68931a5e2390ae21c45678f8c1ddfe10afd736c1
Zia <62405785+ziaualhassain@users.noreply.github.com>
2024-11-12 15:32:59 +0530

    Update Footer.tsx

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
     return (
       <div className="bg-gray-800 w-full text-white py-4 flex items-center justify-center">
-        <span>Thanks You. ❤️</span>
+        <span>Thank You. ❤️</span>
       </div>
     );
   }
